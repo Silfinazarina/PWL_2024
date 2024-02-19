f6ad1c5cc76e958714223d592674cd22bfbf2e65
Silfi Nazarina <silfinazarina@gmail.com>
2024-02-19 20:02:34 +0700

    Modifikasi tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h3>Semangat Belajar Laravel 10</h3>
+    </body>
+</html>
+
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -130,4 +144,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
